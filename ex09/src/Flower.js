d1026bd6d1e05febc6ce950a610ed8cef7c6e9bc
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-29 21:53:22 +0200

    Added ex09

---
+++ ex09/src/Flower.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Flower extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This is a {this.props.color} Flower!</h2>
+            </div>
+        )
+    }
+}
+
+export default Flower;
