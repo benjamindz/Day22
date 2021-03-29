8d743fcf12d566ea5631b9dc18e5cad9c609b1e3
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-29 20:04:36 +0200

    Added ex03

---
+++ ex03/src/Plant.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Plant extends Component {
+    render () {
+        return (
+            <div>
+                <h2>This is a Plant Component!</h2>
+            </div>
+        )
+    }
+}
+
+export default Plant;
